d56870203034d86fd78b0f679c8345886a9afd20
Leonardo Santos <leonardodossantoss50@gmail.com>
2023-04-13 11:35:06 -0300

    Testando tratamento do objeto venda realizada.

--- app_webhook_ticto/urls.py
+++ app_webhook_ticto/urls.py
@@ -3,21 +3,14 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("Abandono-de-Carrinho/", views.Abandono_de_Carrinho, name="Abandono_de_Carrinho"),
-    path("Tempo-de-Teste/", views.Tempo_de_Teste, name="Tempo_de_Teste"),
-    path("Aguardando-Pagamento/", views.Aguardando_Pagamento, name="Aguardando_Pagamento"),
-    path("Encerrado/", views.Encerrado, name="Encerrado"),
-    path("Pix-Expirado/", views.Pix_Expirado, name="Pix_Expirado"),
-    path("Assinatura-Cancelada/", views.Assinatura_Cancelada, name="Assinatura_Cancelada"),
-    path("Pix-Gerado/", views.Pix_Gerado, name="Pix_Gerado"),
-    path("Boleto-Atrasado/", views.Boleto_Atrasado, name="Boleto_Atrasado"),
-    path("Reembolso/", views.Reembolso, name="Reembolso"),
-    path("Boleto-Impresso/", views.Boleto_Impresso, name="Boleto_Impresso"),
     path("Venda-Realizada/", views.Venda_Realizada, name="Venda_Realizada"),
-    path("Chargeback/", views.Chargeback, name="Chargeback"),
     path("Venda-Recusada/", views.Venda_Recusada, name="Venda_Recusada"),
+    path("Chargeback/", views.Chargeback, name="Chargeback"),
+    path("Reembolso/", views.Reembolso, name="Reembolso"),
+    path("Encerrado/", views.Encerrado, name="Encerrado"),
+    path("Assinatura-Cancelada/", views.Assinatura_Cancelada, name="Assinatura_Cancelada"),
+    path("Tempo-de-Teste/", views.Tempo_de_Teste, name="Tempo_de_Teste"),
 ]
-
 # Abandono de Carrinho
 # Tempo de Teste
 # Aguardando Pagamento
--- app_webhook_ticto/views.py
+++ app_webhook_ticto/views.py
@@ -13,19 +13,6 @@ def home(request):
 
 
 @csrf_exempt
-def Abandono_de_Carrinho(request):
-    if request.method == "GET":
-        return JsonResponse({"api": "-"})
-    elif request.method == "POST":
-        try:
-            data = json.loads(request.body)
-            print(f"---> Abandono_de_Carrinho:\n {data}")
-            return JsonResponse(data)
-        except Exception as e:
-            print(f"#### Error | Abandono_de_Carrinho: {e}")
-            return JsonResponse({"code": "500", "msg": e, "local": "Abandono_de_Carrinho"})
-
-@csrf_exempt
 def Tempo_de_Teste(request):
     if request.method == "GET":
         return JsonResponse({"api": "-"})
@@ -37,45 +24,6 @@ def Tempo_de_Teste(request):
         except Exception as e:
             print(f"#### Error | Tempo_de_Teste: {e}")
             return JsonResponse({"code": "500", "msg": e, "local": "Tempo_de_Teste"})
-
-@csrf_exempt
-def Aguardando_Pagamento(request):
-    if request.method == "GET":
-        return JsonResponse({"api": "-"})
-    elif request.method == "POST":
-        try:
-            data = json.loads(request.body)
-            print(f"---> Aguardando_Pagamento:\n {data}")
-            return JsonResponse(data)
-        except Exception as e:
-            print(f"#### Error | Aguardando_Pagamento: {e}")
-            return JsonResponse({"code": "500", "msg": e, "local": "Aguardando_Pagamento"})
-
-@csrf_exempt
-def Encerrado(request):
-    if request.method == "GET":
-        return JsonResponse({"api": "-"})
-    elif request.method == "POST":
-        try:
-            data = json.loads(request.body)
-            print(f"---> Encerrado:\n {data}")
-            return JsonResponse(data)
-        except Exception as e:
-            print(f"#### Error | Encerrado: {e}")
-            return JsonResponse({"code": "500", "msg": e, "local": "Encerrado"})
-
-@csrf_exempt
-def Pix_Expirado(request):
-    if request.method == "GET":
-        return JsonResponse({"api": "-"})
-    elif request.method == "POST":
-        try:
-            data = json.loads(request.body)
-            print(f"---> Pix_Expirado:\n {data}")
-            return JsonResponse(data)
-        except Exception as e:
-            print(f"#### Error | Pix_Expirado: {e}")
-            return JsonResponse({"code": "500", "msg": e, "local": "Pix_Expirado"})
 
 @csrf_exempt
 def Assinatura_Cancelada(request):
@@ -91,32 +39,6 @@ def Assinatura_Cancelada(request):
             return JsonResponse({"code": "500", "msg": e, "local": "Assinatura_Cancelada"})
 
 @csrf_exempt
-def Pix_Gerado(request):
-    if request.method == "GET":
-        return JsonResponse({"api": "-"})
-    elif request.method == "POST":
-        try:
-            data = json.loads(request.body)
-            print(f"---> Pix_Gerado:\n {data}")
-            return JsonResponse(data)
-        except Exception as e:
-            print(f"#### Error | Pix_Gerado: {e}")
-            return JsonResponse({"code": "500", "msg": e, "local": "Pix_Gerado"})
-
-@csrf_exempt
-def Boleto_Atrasado(request):
-    if request.method == "GET":
-        return JsonResponse({"api": "-"})
-    elif request.method == "POST":
-        try:
-            data = json.loads(request.body)
-            print(f"---> Boleto_Atrasado:\n {data}")
-            return JsonResponse(data)
-        except Exception as e:
-            print(f"#### Error | Boleto_Atrasado: {e}")
-            return JsonResponse({"code": "500", "msg": e, "local": "Boleto_Atrasado"})
-
-@csrf_exempt
 def Reembolso(request):
     if request.method == "GET":
         return JsonResponse({"api": "-"})
@@ -130,26 +52,14 @@ def Reembolso(request):
             return JsonResponse({"code": "500", "msg": e, "local": "Reembolso"})
 
 @csrf_exempt
-def Boleto_Impresso(request):
-    if request.method == "GET":
-        return JsonResponse({"api": "-"})
-    elif request.method == "POST":
-        try:
-            data = json.loads(request.body)
-            print(f"---> Boleto_Impresso:\n {data}")
-            return JsonResponse(data)
-        except Exception as e:
-            print(f"#### Error | Boleto_Impresso: {e}")
-            return JsonResponse({"code": "500", "msg": e, "local": "Boleto_Impresso"})
-
-@csrf_exempt
 def Venda_Realizada(request):
     if request.method == "GET":
         return JsonResponse({"api": "-"})
     elif request.method == "POST":
         try:
             data = json.loads(request.body)
-            print(f"---> Venda_Realizada:\n {data}")
+            print(f"---> Venda_Realizada:\n")
+            print(data)
             return JsonResponse(data)
         except Exception as e:
             print(f"#### Error | Venda_Realizada: {e}")
