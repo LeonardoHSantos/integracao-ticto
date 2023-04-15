4068d241c25a8168479b253d0fcde28dcbedd0d8
Leonardo Santos <leonardodossantoss50@gmail.com>
2023-04-15 17:31:40 -0300

    Replace no obj do body para o post de liberação.

--- database/prepare_data.py
+++ database/prepare_data.py
@@ -28,7 +28,7 @@ def prepapre_date(data, process_name):
             "type": process_name,
         }
     }
-    data = json.dumps(data).replace("'", '"')
+    body = json.dumps(body).replace("'", '"')
     try:
         requests.post(url=URL_LIBERACAO,data=body)
         print(f"REQUEST | STATUS: OK | BODY: {body}")
